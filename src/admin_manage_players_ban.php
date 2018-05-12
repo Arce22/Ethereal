4700b170453b24164ddb97760273873d88ef3f2c
bikemc <bikemcamli@gmail.com>
2018-05-13 00:05:12 +0300

    admin manage players

---
+++ src/admin_manage_players_ban.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require 'database.php';
+if( isset($_SESSION['admin_id'])) {
+  header("Location: ");
+}
+//including the database connection file
+
+ 
+//getting id of the data from url
+$player_id = $_GET['player_id'];
+ 
+//deleting the row from table4
+  $records = $conn->prepare('insert into banned values (:player_id,:admin_id)'); // = ' .$_SESSION['company_id'].);
+            $records->bindParam(':player_id', $player_id);
+          	 $records->bindParam(':admin_id',$_SESSION['admin_id']);
+            $records->execute();
+
+
+//redirecting to the display page (index.php in our case)
+header("Location:admin_manage_players.php");
+
+?>
