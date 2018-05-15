89b891b33829b7a8225f100c8d2ecdf5cd57f8a1
bikemc <bikemcamli@gmail.com>
2018-05-15 08:42:46 +0300

    admin_games link cancelled

--- src/admin_manage_comments.php
+++ src/admin_manage_comments.php
@@ -135,7 +135,7 @@ if(!empty($_POST['disapprove_comment']) && !empty($_POST['comment_id']))
     <thead>
     <tr>
         <td><a href="admin_market.php"><button type="button" style="float: right;" class="btn_name">Market Place</button></td>
-        <td><a href="admin_games.php"><button type="button" style="float: right;" class="btn_name">Games</button></td>
+       
         <td><a href="admin_profile.php"><button type="button" style="float: right;" class="btn_name">Profile</button></td>
     </tr>
     <thead>
